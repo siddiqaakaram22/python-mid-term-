2a571ffd759505fa85f88b3eefc8e6c8c4c85d1c
siddiqaakaram22 <156875719+siddiqaakaram22@users.noreply.github.com>
2024-01-17 16:29:29 +0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,21 @@
+a=int (input("enter one"))
+b=int (input("enter three"))
+c= input("enter a sign")
+if c == "+":
+    print(a+b)
+
+a=int (input("enter four"))
+b=int (input("enter three"))
+c= input("enter a sign")
+if c == "-":
+    print(a-b)
+a=int (input("enter five"))
+b=int (input("enter three"))
+c= input("enter a sign")
+if c == "*":
+    print(a*b)
+a=int (input("enter six"))
+b=int (input("enter three"))
+c= input("enter a sign")
+if c == "/":
+    print(a/b)
